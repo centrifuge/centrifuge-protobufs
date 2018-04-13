260599d70584ed960c0c2feaae086c82cdfe6e11
Lucas Vogelsang <l@lucasvo.com>
2018-04-13 15:08:41 +0200

    Configured prototool & added InvoiceDataSalts & renaming Data object (#2)

---
+++ scripts/install_prototool.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+
+#PROTOTOOL_VERSION=0.1.0
+PROTOTOOL_BIN=~/bin/$PROTOTOOL_VERSION
+mkdir -p ~/bin/$PROTOTOOL_VERSION/
+
+if [ -e "${PROTOTOOL_BIN}/prototool" ]
+then
+    echo "Found existing prototool in ${PROTOTOOL_BIN}. Not downloading again - just linking."
+else
+    echo "Downloading prototool"
+    curl -sSL https://github.com/uber/prototool/releases/download/v$PROTOTOOL_VERSION/prototool-$(uname -s)-$(uname -m) > $PROTOTOOL_BIN/prototool && chmod +x $PROTOTOOL_BIN/prototool && $PROTOTOOL_BIN/prototool -h
+fi
