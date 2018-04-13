947c0225723c660e4b2b206fb3562e149b2a96a7
Lucas Vogelsang <l@lucasvo.com>
2018-04-13 15:05:44 +0200

    Install script: pass in env var

--- scripts/install_prototool.sh
+++ scripts/install_prototool.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env sh
 
-PROTOTOOL_BIN=~/bin/$PROTOTOOL_VERSION/
-mkdir -p ~/bin/$PROTOTOOL_VERSION
+#PROTOTOOL_VERSION=0.1.0
+PROTOTOOL_BIN=~/bin/$PROTOTOOL_VERSION
+mkdir -p ~/bin/$PROTOTOOL_VERSION/
 
 if [ -e "${PROTOTOOL_BIN}/prototool" ]
 then
     echo "Found existing prototool in ${PROTOTOOL_BIN}. Not downloading again - just linking."
 else
     echo "Downloading prototool"
-    curl -sSL https://github.com/uber/prototool/releases/download/v0.1.0/prototool-$(uname -s)-$(uname -m) > $PROTOTOOL_BIN/prototool && chmod +x $PROTOTOOL_BIN/prototool && $PROTOTOOL_BIN/prototool-h
+    curl -sSL https://github.com/uber/prototool/releases/download/v$PROTOTOOL_VERSION/prototool-$(uname -s)-$(uname -m) > $PROTOTOOL_BIN/prototool && chmod +x $PROTOTOOL_BIN/prototool && $PROTOTOOL_BIN/prototool -h
 fi
