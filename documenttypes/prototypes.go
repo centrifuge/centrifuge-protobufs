b31a03e7cf251c27fe00e83d254e3feffe792887
Lucas Vogelsang <l@lucasvo.com>
2018-05-11 09:16:57 -0400

    cleanup

--- documenttypes/prototypes.go
+++ documenttypes/prototypes.go
@@ -4,5 +4,4 @@ const (
 	InvoiceDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceDocument"
 	InvoiceDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceData"
 	InvoiceSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceSalts"
-
 )
