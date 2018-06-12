c54c484c680a98e7da036e69f3c16865fbb4770e
Lucas Vogelsang <l@lucasvo.com>
2018-06-11 20:36:58 -0400

    CreateProof RPC & PurchaseOrder Service (#9)
    * Adding proof endpoint
    
    * Fix swagger options
    
    * Rename fields
    
    * URL changes
    
    * Added purchaseorder endpoints

--- gen/go/invoice/service.pb.gw.go
+++ gen/go/invoice/service.pb.gw.go
@@ -29,6 +29,37 @@ var _ status.Status
 var _ = runtime.String
 var _ = utilities.NewDoubleArray
 
+func request_InvoiceDocumentService_CreateInvoiceProof_0(ctx context.Context, marshaler runtime.Marshaler, client InvoiceDocumentServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq CreateInvoiceProofEnvelope
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	var (
+		val string
+		ok  bool
+		err error
+		_   = err
+	)
+
+	val, ok = pathParams["document_identifier"]
+	if !ok {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "missing parameter %s", "document_identifier")
+	}
+
+	protoReq.DocumentIdentifier, err = runtime.Bytes(val)
+
+	if err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "type mismatch, parameter: %s, error: %v", "document_identifier", err)
+	}
+
+	msg, err := client.CreateInvoiceProof(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_InvoiceDocumentService_AnchorInvoiceDocument_0(ctx context.Context, marshaler runtime.Marshaler, client InvoiceDocumentServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq AnchorInvoiceEnvelope
 	var metadata runtime.ServerMetadata
@@ -128,6 +159,35 @@ func RegisterInvoiceDocumentServiceHandler(ctx context.Context, mux *runtime.Ser
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
 // "InvoiceDocumentServiceClient" to call the correct interceptors.
 func RegisterInvoiceDocumentServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client InvoiceDocumentServiceClient) error {
+
+	mux.Handle("POST", pattern_InvoiceDocumentService_CreateInvoiceProof_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_InvoiceDocumentService_CreateInvoiceProof_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_InvoiceDocumentService_CreateInvoiceProof_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
 
 	mux.Handle("POST", pattern_InvoiceDocumentService_AnchorInvoiceDocument_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
@@ -249,16 +309,20 @@ func RegisterInvoiceDocumentServiceHandlerClient(ctx context.Context, mux *runti
 }
 
 var (
+	pattern_InvoiceDocumentService_CreateInvoiceProof_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1, 2, 2}, []string{"invoice", "document_identifier", "proof"}, ""))
+
 	pattern_InvoiceDocumentService_AnchorInvoiceDocument_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"invoice", "anchor"}, ""))
 
 	pattern_InvoiceDocumentService_SendInvoiceDocument_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"invoice", "send"}, ""))
 
-	pattern_InvoiceDocumentService_GetInvoiceDocument_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2}, []string{"invoice", "get", "document_identifier"}, ""))
+	pattern_InvoiceDocumentService_GetInvoiceDocument_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 1, 0, 4, 1, 5, 1}, []string{"invoice", "document_identifier"}, ""))
 
 	pattern_InvoiceDocumentService_GetReceivedInvoiceDocuments_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"invoice", "getReceived"}, ""))
 )
 
 var (
+	forward_InvoiceDocumentService_CreateInvoiceProof_0 = runtime.ForwardResponseMessage
+
 	forward_InvoiceDocumentService_AnchorInvoiceDocument_0 = runtime.ForwardResponseMessage
 
 	forward_InvoiceDocumentService_SendInvoiceDocument_0 = runtime.ForwardResponseMessage
