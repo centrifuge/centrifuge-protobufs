c772adc01eb713325fa2e671e2467d9ccf6cc80a
Lucas Vogelsang <l@lucasvo.com>
2018-06-11 22:36:14 -0400

    Adding purchase order types

--- documenttypes/prototypes.go
+++ documenttypes/prototypes.go
@@ -4,4 +4,7 @@ const (
 	InvoiceDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceDocument"
 	InvoiceDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceData"
 	InvoiceSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceSalts"
+	PurchaseOrderDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderDocument"
+	PurchaseOrderDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderData"
+	PurchaseOrderSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderSalts"
 )
