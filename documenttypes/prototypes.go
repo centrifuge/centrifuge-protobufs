dd14a9543f91e885b901c595b7981aad6174c253
Manuel Polzhofer <manuel.polzhofer@gmail.com>
2018-08-30 15:28:17 +0200

    goimports formatting

--- documenttypes/prototypes.go
+++ documenttypes/prototypes.go
@@ -1,10 +1,10 @@
 package documenttypes
 
 const (
-	InvoiceDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceDocument"
-	InvoiceDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceData"
-	InvoiceSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceSalts"
+	InvoiceDocumentTypeUrl       = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceDocument"
+	InvoiceDataTypeUrl           = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceData"
+	InvoiceSaltsTypeUrl          = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceSalts"
 	PurchaseOrderDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderDocument"
-	PurchaseOrderDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderData"
-	PurchaseOrderSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderSalts"
+	PurchaseOrderDataTypeUrl     = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderData"
+	PurchaseOrderSaltsTypeUrl    = "http://github.com/centrifuge/centrifuge-protobufs/purchaseorder/#purchaseorder.PurchaseOrderSalts"
 )
