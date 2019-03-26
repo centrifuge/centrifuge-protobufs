fb40eb56d420258c9b653b92f2b5364f71d4cd8d
Charly <fei.charleen@gmail.com>
2019-03-26 18:23:00 +0100

    Add EntityRelationship TypeUrls (#96)
    * added entity relationship
    
    * update per fix comment

--- documenttypes/prototypes.go
+++ documenttypes/prototypes.go
@@ -10,4 +10,7 @@ const (
 	EntityDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntityDocument"
 	EntityDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntityData"
 	EntitySaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntitySalts"
+	EntityRelationshipDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipDocument"
+	EntityRelationshipDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipData"
+	EntityRelationshipSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipSalts"
 )
