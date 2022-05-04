787f9fbe24bcc54235a42ed514e1dbe7e5a5c5a5
cdamian <17934949+cdamian@users.noreply.github.com>
2022-05-04 12:40:48 +0200

    core: Add new message for NFTs

--- documenttypes/prototypes.go
+++ documenttypes/prototypes.go
@@ -1,14 +1,18 @@
 package documenttypes
 
+import (
+	_ "github.com/centrifuge/precise-proofs/proofs/proto"
+)
+
 const (
-	InvoiceDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceDocument"
-	InvoiceDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceData"
-	InvoiceSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceSalts"
-	EntityDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntityDocument"
-	EntityDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntityData"
-	EntitySaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntitySalts"
+	InvoiceDocumentTypeUrl            = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceDocument"
+	InvoiceDataTypeUrl                = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceData"
+	InvoiceSaltsTypeUrl               = "http://github.com/centrifuge/centrifuge-protobufs/invoice/#invoice.InvoiceSalts"
+	EntityDocumentTypeUrl             = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntityDocument"
+	EntityDataTypeUrl                 = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntityData"
+	EntitySaltsTypeUrl                = "http://github.com/centrifuge/centrifuge-protobufs/entity/#entity.EntitySalts"
 	EntityRelationshipDocumentTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipDocument"
-	EntityRelationshipDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipData"
-	EntityRelationshipSaltsTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipSalts"
-	GenericDataTypeUrl = "http://github.com/centrifuge/centrifuge-protobufs/generic/#generic.Generic"
+	EntityRelationshipDataTypeUrl     = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipData"
+	EntityRelationshipSaltsTypeUrl    = "http://github.com/centrifuge/centrifuge-protobufs/entityrelationship/#entityrelationship.EntityRelationshipSalts"
+	GenericDataTypeUrl                = "http://github.com/centrifuge/centrifuge-protobufs/generic/#generic.Generic"
 )
--- go.mod
+++ go.mod
@@ -2,11 +2,8 @@ module github.com/centrifuge/centrifuge-protobufs
 
 go 1.18
 
-require (
-	github.com/centrifuge/precise-proofs v0.0.0-20190731143435-2ed0cc3986aa
-	github.com/golang/protobuf v1.5.0
-)
+require github.com/centrifuge/precise-proofs v0.0.0-20190731143435-2ed0cc3986aa
 
 require google.golang.org/protobuf v1.28.0
 
-replace github.com/centrifuge/precise-proofs => github.com/cdamian/precise-proofs v0.0.0-20220429153446-f12245bdfe21
+replace github.com/centrifuge/precise-proofs => github.com/cdamian/precise-proofs v0.0.0-20220503104139-49f63528e480
